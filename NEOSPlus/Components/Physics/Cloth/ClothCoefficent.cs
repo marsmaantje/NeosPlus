496f511a591b2fb4965354b24e1204db6cdd3f73
xLinka <Oliver.dunnington@outlook.com>
2023-04-19 16:07:42 +0100

    Removal of ClothSim to allow headless

--- NEOSPlus/Components/Physics/Cloth/ClothCoefficent.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace FrooxEngine
-{
-    public class ClothCoefficent : SyncObject
-    {
-        public readonly Sync<ulong> VertexIndex;
-        public readonly Sync<float> MaxDistance;
-        public readonly Sync<float> CollisionSphereDistance;
-    }
-}
